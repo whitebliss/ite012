3216f0c696904fc6f8b38791ede249f34d609929
miyagi69 <71875825+miyagi69@users.noreply.github.com>
2020-09-28 18:29:27 +0800

    Add files via upload

--- AbreraBonifacioCaperalCunananTacsuan/src/Module4Pdf/Open_PDF.java
+++ AbreraBonifacioCaperalCunananTacsuan/src/Module4Pdf/Open_PDF.java
@@ -1,4 +1,4 @@
-package Module4Pdf;
+package OpenPDF;
 
 import java.awt.Desktop;
 import java.awt.EventQueue;
@@ -11,9 +11,9 @@ import java.awt.Font;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Color;
 import javax.swing.SwingConstants;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Open_PDF {
 
@@ -49,66 +49,132 @@ public class Open_PDF {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(255, 192, 203));
 		frame.setBounds(100, 100, 478, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//pdf module 4
-				JButton btnNewButton = new JButton("EUROPEAN ART HISTORY");
-				btnNewButton.setForeground(new Color(255, 105, 180));
-				btnNewButton.setBackground(new Color(255, 245, 238));
-				btnNewButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						
-						try {
-							Desktop.getDesktop().open(new java.io.File("PDF/NOTES. EUROPEAN ART HISTORY.pdf"));
-						}
-						catch (Exception e1) { e1.printStackTrace();
-							JoptionPane.showMessageDialog(null, "Check your file details");
-							
-						}
-					}
-				});
-				btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		JButton btnNewButton = new JButton("EUROPEAN ART HISTORY");
+		btnNewButton.setBackground(new Color(238, 130, 238));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				
-				//pdf module 4
-				JButton btnNewButton_1 = new JButton("ART HISTORY. EGYPT AND ROME");
-				btnNewButton_1.setForeground(new Color(255, 105, 180));
-				btnNewButton_1.setBackground(new Color(255, 245, 238));
-				btnNewButton_1.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						
-						try {
-							Desktop.getDesktop().open(new java.io.File("PDF/NOTES.ART HISTORY. EGYPT AND ROME.pdf"));
-						}
-						catch (Exception e1) { e1.printStackTrace();
-							JoptionPane.showMessageDialog(null, "Check your file details");
-							
-						}
-					}
-				});
-				btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+				try {
+					Desktop.getDesktop().open(new java.io.File("PDF/NOTES. EUROPEAN ART HISTORY.pdf"));
+				}
+				catch (Exception e1) { e1.printStackTrace();
+					JoptionPane.showMessageDialog(null, "Check your file details");
+					
+				}
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 10));
+		
+		//pdf module 4
+		JButton btnNewButton_1 = new JButton("ART HISTORY. EGYPT AND ROME");
+		btnNewButton_1.setBackground(new Color(238, 130, 238));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					Desktop.getDesktop().open(new java.io.File("PDF/NOTES.ART HISTORY. EGYPT AND ROME.pdf"));
+				}
+				catch (Exception e1) { e1.printStackTrace();
+					JoptionPane.showMessageDialog(null, "Check your file details");
+					
+				}
+			}
+		});
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.BOLD, 10));
+		
+		//Label mod4
+		JLabel lblNewLabel = new JLabel("PDFs for Module 4");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(255, 0, 255));
+		lblNewLabel.setBackground(new Color(0, 0, 128));
+		lblNewLabel.setFont(new Font("Serif", Font.BOLD, 20));
+		
+		//eto yung pdf sa module 3
+		JButton btnNewButton_2 = new JButton("ART-SUBJECT CONTENT");
+		btnNewButton_2.setBackground(new Color(238, 130, 238));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					Desktop.getDesktop().open(new java.io.File("PDF/Art-Subject_Content.pdf"));
+				}
+				catch (Exception e1) { e1.printStackTrace();
+					JoptionPane.showMessageDialog(null, "Check your file details");
+				}
+			}
+		});
+		btnNewButton_2.setForeground(new Color(0, 0, 0));
+		btnNewButton_2.setFont(new Font("Times New Roman", Font.BOLD, 10));
+		
+		//Label mod3
+		JLabel lblNewLabel_1 = new JLabel("PDF for Module 3");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setForeground(new Color(255, 0, 255));
+		lblNewLabel_1.setBackground(new Color(0, 0, 128));
+		lblNewLabel_1.setFont(new Font("Serif", Font.BOLD, 20));
+		
+		//Title (Ikaw na bahala sa design or kung gusto mo pa ilagay 'to)
+		JLabel lblNewLabel_2 = new JLabel("Art Appreciation");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		
+		//<-Back Button
+		JButton btnNewButton_3 = new JButton("<- Back");
+		btnNewButton_3.setFont(new Font("Arial", Font.BOLD, 10));
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//di pa ako marunong paano gawin 'to mwehehehe
+			}
+		});
 		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(245, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(221, Short.MAX_VALUE)
-					.addComponent(btnNewButton_1)
+					.addContainerGap(12, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 156, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 163, GroupLayout.PREFERRED_SIZE))
+							.addGap(108)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+									.addComponent(btnNewButton_3)
+									.addComponent(btnNewButton, Alignment.LEADING))))
+						.addComponent(btnNewButton_1))
 					.addContainerGap())
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(44)
+					.addComponent(lblNewLabel_2, GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE)
+					.addGap(52))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(25)
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
-					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
-					.addGap(28))
+					.addContainerGap()
+					.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))
+					.addGap(11)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(31)
+							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(64)
+							.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(18, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}
