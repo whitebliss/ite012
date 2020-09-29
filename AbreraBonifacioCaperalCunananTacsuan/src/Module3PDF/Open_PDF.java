dfcf789a21dc669f03b92b997a95e3bc10e7cb7d
ARMANGPINAS <71876088+ARMANGPINAS@users.noreply.github.com>
2020-09-29 08:24:48 +0800

    Add files via upload

--- AbreraBonifacioCaperalCunananTacsuan/src/Module3PDF/Open_PDF.java
+++ AbreraBonifacioCaperalCunananTacsuan/src/Module3PDF/Open_PDF.java
@@ -1,6 +1,6 @@
 package Module3PDF;
 
-import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -8,6 +8,15 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Color;
+
+
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+import Module3Quiz.Quiz;
+
 
 public class Open_PDF {
 
@@ -43,28 +52,68 @@ public class Open_PDF {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(new Color(255, 192, 203));
+		frame.setBounds(100, 100, 478, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Art Subject and Content");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnNewButton.setForeground(new Color(255, 105, 180));
-		btnNewButton.setBackground(new Color(255,245,238));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		//pdf module 4
+				JButton btnNewButton = new JButton("ART SUBJECT AND CONTENT");
+				btnNewButton.setForeground(new Color(255, 105, 180));
+				btnNewButton.setBackground(new Color(255, 245, 238));
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+						try {
+							Runtime.getRuntime().exec("rundll32 url.dll, FileProtocolHandler " + "ArtSubject.pdf");
+						}
+						catch (Exception e1) { e1.printStackTrace();
+							JoptionPane.showMessageDialog(null, "Check your file details");
+							
+						}
+					}
+				});
+				btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
 				
-				try {
-					Runtime.getRuntime().exec("rundll32 url.dll, FileProtocolHandler " + "ArtSubject.pdf");
-				}
-				catch (Exception e1) {
-					JoptionPane.showMessageDialog(null, "Check your file details");
-					
-				}
+		
+		
+				//button for taking the quiz
+		JButton btnNewButton_2 = new JButton("Take the quiz now!");
+		btnNewButton_2.setForeground(new Color(255, 105, 180));
+		btnNewButton_2.setBackground(new Color(255, 245, 238));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				Quiz qu = new Quiz();
+				qu.main(null);
 			}
 		});
-		btnNewButton.setBounds(41, 35, 356, 187);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
+		
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)						
+							.addGroup(groupLayout.createSequentialGroup()
+								.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap(245, Short.MAX_VALUE)))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(btnNewButton_2)
+							.addContainerGap())))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(25)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		frame.getContentPane().setLayout(groupLayout);
 	}
 
 	protected void showMessageDialog(Object object, String string) {
